939da4bac1ece163741a89a2864bc2392171177c
Lili Manrique <liliana.manrique1@gmail.com>
2020-07-03 10:55:36 -0600

    Use state to track url input and add dummy onClick method to button

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import './SearchForm.css';
+import React, { useState } from "react";
+import "./SearchForm.css";
 
 const SearchForm = () => {
+  const [url, setUrl] = useState("");
+
+  const handleInputChange = e => {
+    setUrl(e.target.value);
+  };
 
   return (
     <div>
-      <input type="text" placeholder="enter a .vtt file url here" />
-      <button>load file</button>
+      <input
+        type="text"
+        placeholder="enter a .vtt file url here"
+        value={url}
+        onChange={(e) => handleInputChange(e)}
+      />
+      <button onClick={() => console.log(url)}>load file</button>
     </div>
   );
-}
+};
 
 export default SearchForm;
