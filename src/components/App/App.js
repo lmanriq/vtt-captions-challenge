715ad2546a4d50077a54cc9f6cb87c859005afd6
Lili Manrique <liliana.manrique1@gmail.com>
2020-07-03 11:10:39 -0600

    Pass url as props to CueContainer and print basic render of container compoment.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
-import './App.css';
-import SearchForm from '../SearchForm/SearchForm';
+import React, { useState } from "react";
+import "./App.css";
+import SearchForm from "../SearchForm/SearchForm";
+import CueContainer from "../CueContainer/CueContainer";
 
 const App = () => {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState("");
 
-  const saveUrl = url => {
+  const saveUrl = (url) => {
     setUrl(url);
-  }
+  };
 
   return (
     <div>
       <h1>VTT Caption Editor</h1>
-      <SearchForm saveUrl={saveUrl}/>
+      <SearchForm saveUrl={saveUrl} />
+      <CueContainer url={url} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/CueContainer/CueContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CueContainer.css";
+
+const CueContainer = ({ url }) => {
+return (
+    <div>
+      {url && url}
+    </div>
+  );
+};
+
+export default CueContainer;
