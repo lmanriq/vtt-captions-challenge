69662f2a661d4aaa138f846ff4b4d381ebadeb93
Lili Manrique <liliana.manrique1@gmail.com>
2020-07-05 14:10:37 -0600

    Add basic url fetch within useEffect to get the vtt response back from the file

--- src/components/CueContainer/CueContainer.js
+++ src/components/CueContainer/CueContainer.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./CueContainer.css";
 
 const CueContainer = ({ url }) => {
-return (
-    <div>
-      {url && url}
-    </div>
-  );
+  const [captions, setCaptions] = useState("");
+
+  useEffect(() => {
+    if (url) {
+      fetchData(url);
+    }
+  }, [url]);
+
+  const fetchData = async (url) => {
+    console.log('enter')
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`, {
+      headers: {
+        "Content-Type": "text/vtt",
+      },
+    });
+    console.log(response)
+  };
+
+  return <div>{url && url}</div>;
 };
 
 export default CueContainer;
