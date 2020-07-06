66adbf9f47917dc00746f620ef83534c72b83d9b
Lili Manrique <liliana.manrique1@gmail.com>
2020-07-06 15:14:10 -0600

    Add render test for app title.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders what we expect', () => {
+    const { getByText } = render(<App />)
+    expect(getByText('VTT Caption Editor')).toBeInTheDocument();
+  });
 });
