6df4e03f2e26285fdfa66416df9c28fe8510831a
Lili Manrique <liliana.manrique1@gmail.com>
2020-07-06 16:07:44 -0600

    Add App integration test for loading data.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-import '@testing-library/jest-dom';
+import React from "react";
+import { render, waitForElement, fireEvent } from "@testing-library/react";
+import App from "./App";
+import "@testing-library/jest-dom";
+import { fetchVttFile } from "../../apiCalls";
+jest.mock("../../apiCalls");
 
-describe('App', () => {
-  it('renders what we expect', () => {
-    const { getByText } = render(<App />)
-    expect(getByText('VTT Caption Editor')).toBeInTheDocument();
+describe("App", () => {
+  it("renders what we expect", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("VTT Caption Editor")).toBeInTheDocument();
+  });
+
+  it("can load data", async () => {
+    const mockVtt = `WEBVTT
+
+00:01.000 --> 00:04.000
+Never drink liquid nitrogen.
+
+00:05.000 --> 00:09.000
+It will perforate your stomach.`;
+    fetchVttFile.mockResolvedValueOnce(mockVtt);
+    const { getByText, getByPlaceholderText } = render(<App />);
+
+
+    const input = getByPlaceholderText("enter a .vtt file url here");
+    fireEvent.change(input, { target: { value: "file.vtt" } });
+    const loadBtn = getByText("load file");
+    fireEvent.click(loadBtn);    
+    const sampleCaption = await waitForElement(() => getByText("Never drink liquid nitrogen."))
+    expect(sampleCaption).toBeInTheDocument();
   });
 });
